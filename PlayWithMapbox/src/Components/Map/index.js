8c73f766f5562ebfa673921b66770955d59477b9
tayathorn <tayathorn@thinknet.co.th>
2017-09-28 13:14:38 +0700

    set center map and zoom level when user location has changed

--- PlayWithMapbox/src/Components/Map/index.js
+++ PlayWithMapbox/src/Components/Map/index.js
@@ -12,9 +12,20 @@ const accessToken = Config.map.accessToken
 Mapbox.setAccessToken(accessToken);
 
 export default class Map extends Component {
+
+  setZoomLevel = (zoomLevel, animated = true, callback = ()=>{}) => {
+    // console.log('setZoomLevel')
+    this._map.setZoomLevel(zoomLevel, animated, callback)
+  }
+
+  setCenterCoordinateZoomLevel = (latitude, longitude, zoomLevel, animated = true, callback = ()=>{}) => {
+    this._map.setCenterCoordinateZoomLevel(latitude, longitude, zoomLevel, animated, callback)
+  }
+
   render() {
     return(
       <MapView 
+        ref={map => { this._map = map; }}
         {...this.props}
       />
     )
--- PlayWithMapbox/src/Containers/Home/index.js
+++ PlayWithMapbox/src/Containers/Home/index.js
@@ -8,20 +8,44 @@ import {
 import Map from '../../Components/Map'
 import { Config } from '../../config'
 
+const initialMap = {
+  zoom: 0,
+  center: {
+    latitude: 0,
+    longitude: 0,
+  },
+}
+
+const ZOOM_LEVEL = 15
+
 export default class Home extends Component {
+  constructor() {
+    super()
+  }
+
+  onUpdateUserLocation = (res) => {
+
+    let { latitude, longitude } = res
+    this._map.setCenterCoordinateZoomLevel(latitude, longitude, ZOOM_LEVEL, true)
+    
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
         <Map
           ref={map => { this._map = map; }}
           style={{ flex: 1 }}
-          initialZoomLevel={0}
+          initialZoomLevel={initialMap.zoom}
+          initialCenterCoordinate={initialMap.center}
           initialDirection={0}
           rotateEnabled={true}
           scrollEnabled={true}
+          pitchEnabled={false}
           zoomEnabled={true}
-          showsUserLocation={false}
+          showsUserLocation={true}
           styleURL={Config.map.styleUrl}
+          onUpdateUserLocation={this.onUpdateUserLocation}
         />
       </View>
     )
