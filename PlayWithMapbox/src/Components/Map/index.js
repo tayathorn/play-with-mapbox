03a289a32783ed723dc8d372a022090f559ab3b8
tayathorn <tayathorn@thinknet.co.th>
2017-09-28 16:04:23 +0700

    add gif image into custom annotation

--- PlayWithMapbox/src/Components/Map/index.js
+++ PlayWithMapbox/src/Components/Map/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
 import Mapbox, { MapView, Annotation } from 'react-native-mapbox-gl'
@@ -11,29 +12,61 @@ import { Config } from '../../config'
 const accessToken = Config.map.accessToken
 Mapbox.setAccessToken(accessToken);
 
+const initialMap = {
+  zoom: 0,
+  center: {
+    latitude: 0,
+    longitude: 0,
+  },
+}
+
+const ZOOM_LEVEL = 12
+
 export default class Map extends Component {
 
-  setZoomLevel = (zoomLevel, animated = true, callback = ()=>{}) => {
-    this._map.setZoomLevel(zoomLevel, animated, callback)
+  constructor(props) {
+    super(props)
   }
 
-  setCenterCoordinateZoomLevel = (latitude, longitude, zoomLevel, animated = true, callback = ()=>{}) => {
-    this._map.setCenterCoordinateZoomLevel(latitude, longitude, zoomLevel, animated, callback)
+  onUpdateUserLocation = (location) => {
+
+    let { latitude, longitude } = location
+    this._map.setCenterCoordinateZoomLevel(latitude, longitude, ZOOM_LEVEL, true)
+
+  }
+
+  onRegionDidChange = (location) => {
+    if(this.props.onRegionDidChange) {
+      this.props.onRegionDidChange(location)
+    }
   }
 
   render() {
     return(
       <MapView 
         ref={map => { this._map = map; }}
-        {...this.props}
+        style={{ flex: 1 }}
+        initialZoomLevel={initialMap.zoom}
+        initialCenterCoordinate={initialMap.center}
+        initialDirection={0}
+        rotateEnabled={true}
+        scrollEnabled={true}
+        pitchEnabled={false}
+        zoomEnabled={true}
+        showsUserLocation={true}
+        styleURL={Config.map.styleUrl}
+        onUpdateUserLocation={this.onUpdateUserLocation}
+        onRegionDidChange={this.onRegionDidChange}
+        logoIsHidden={true}
+        attributionButtonIsHidden={true}
       >
         <Annotation
           id="annotation1"
           coordinate={{latitude: 13.726286, longitude: 100.5252063057}}
           style={{alignItems: 'center', justifyContent: 'center', position: 'absolute'}}
         >
-        <View style={{width: 100, height: 100, borderWidth: 4, borderColor: 'blue', borderRadius: 50, backgroundColor: 'white', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>React View</Text>
+        <View style={{width: 100, height: 100}}>
+          <Image style={{width: 100, height: 100}} source={require('../../images/gif/NPC_325.gif')}/>
         </View>
         </Annotation>
       </MapView>
--- PlayWithMapbox/src/Containers/Home/index.js
+++ PlayWithMapbox/src/Containers/Home/index.js
@@ -10,19 +10,9 @@ import { Config } from '../../config'
 import Map from '../../Components/Map'
 import CenterDetail from '../../Components/Map/CenterDetail'
 
-const initialMap = {
-  zoom: 0,
-  center: {
-    latitude: 0,
-    longitude: 0,
-  },
-}
-
-const ZOOM_LEVEL = 15
-
 export default class Home extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
       centerMapDetail: {
@@ -31,28 +21,10 @@ export default class Home extends Component {
         zoomLevel: 0
       }
     }
-
-    this.centerMapDetail = {
-      latitude: 0,
-      longitude: 0,
-      zoomLevel: 0
-    }
-  }
-
-  onUpdateUserLocation = (location) => {
-
-    let { latitude, longitude } = location
-    this._map.setCenterCoordinateZoomLevel(latitude, longitude, ZOOM_LEVEL, true)
-
   }
 
   onRegionDidChange = (location) => {
     let { latitude, longitude, zoomLevel } = location
-
-    this.centerMapDetail = {
-      latitude, longitude, zoomLevel
-    }
-
     this.setState({
       centerMapDetail: {
         latitude, longitude, zoomLevel
@@ -60,31 +32,13 @@ export default class Home extends Component {
     })
   }
 
-  getAnnotation = () => {
-    
-  }
-
   render() {
     return (
       <View style={{ flex: 1 }}>
         <Map
-          ref={map => { this._map = map; }}
-          style={{ flex: 1 }}
-          initialZoomLevel={initialMap.zoom}
-          initialCenterCoordinate={initialMap.center}
-          initialDirection={0}
-          rotateEnabled={true}
-          scrollEnabled={true}
-          pitchEnabled={false}
-          zoomEnabled={true}
-          showsUserLocation={true}
-          styleURL={Config.map.styleUrl}
-          onUpdateUserLocation={this.onUpdateUserLocation}
           onRegionDidChange={this.onRegionDidChange}
-          logoIsHidden={true}
-          attributionButtonIsHidden={true}
         />
-        <CenterDetail center={this.centerMapDetail} />
+        <CenterDetail center={this.state.centerMapDetail} />
       </View>
     )
   }
